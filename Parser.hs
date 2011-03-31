e71a172fd4e6e9409dcf4c291bda452a74e151fa
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-03-31 16:33:02 +0100

    Lexical analyzer and parser for the core language of VL.

---
+++ Parser.hs
@@ -0,0 +1,92 @@
+module VL.Parser where
+
+import VL.Common
+import VL.Scalar
+
+import VL.Token (Token)
+import qualified VL.Token as Token
+
+import Text.ParserCombinators.Parsec hiding (many, optional, (<|>), Parser)
+import Control.Applicative
+
+data Expression
+    = Variable Name
+    | Constant Scalar
+    | Lambda Name Expression
+    | Application Expression Expression
+    | Cons Expression Expression
+      deriving (Show)
+
+type Parser a = GenParser Token () a
+
+-- @extract selector@ is a parser that consumes one token @t@ and
+-- fails if @selector t@ is @Nothing@ or returns @x@ such that
+-- @selector t == Just x@.
+extract :: (Token -> Maybe a) -> Parser a
+extract selector = tokenPrim show (\pos _ _ -> pos) selector
+
+-- @keyword k@ is a parser that succeeds if the next token is an
+-- identifier @k@ and returns @k@, or fails otherwise.
+keyword :: String -> Parser String
+keyword k = extract getKeyword
+    where
+      getKeyword (Token.Identifier n)
+          | k == n    = Just k
+          | otherwise = Nothing
+      getKeyword _    = Nothing
+
+-- @literate t@ is a parser that succeeds if the next token is equal
+-- to the supplied token @t@ and returns @t@, or fails otherwise.
+literate :: Token -> Parser Token
+literate t = extract getLiterate
+    where
+      getLiterate x
+          | x == t    = Just x
+          | otherwise = Nothing
+
+-- @identifier@ is a parser that succeeds if the next token is an
+-- identifier that is not a keyword and returns the name of that
+-- identifier, or fails otherwise.
+identifier :: Parser String
+identifier = extract getIdentifier <?> "identifier"
+    where
+      getIdentifier (Token.Identifier x)
+          | x `notElem` keywords = Just x
+          | otherwise            = Nothing
+      getIdentifier _            = Nothing
+
+variable :: Parser Expression
+variable = Variable <$> identifier
+
+keywords :: [String]
+keywords = ["lambda", "cons"]
+
+-- @constant@ is a parser that succeeds if the next token is a
+-- constant (i.e., nil, #t, #f, or a real) and returns it
+-- suitably wrapped, or fails otherwise.
+constant :: Parser Expression
+constant = Constant <$> (try emptyList <|> extract getConstant) <?> "constant"
+    where
+      getConstant (Token.Boolean b) = Just (Boolean b)
+      getConstant (Token.Real    r) = Just (Real    r)
+      getConstant _                 = Nothing
+
+emptyList :: Parser Scalar
+emptyList = Nil <$ (literate Token.Quote >> literate Token.LParen >> literate Token.RParen)
+
+parens :: Parser a -> Parser a
+parens = between (literate Token.LParen) (literate Token.RParen)
+
+lambda :: Parser Expression
+lambda = liftA2 Lambda (keyword "lambda" *> parens identifier) expression
+
+application :: Parser Expression
+application = liftA2 Application expression expression
+
+cons :: Parser Expression
+cons = liftA2 Cons (keyword "cons" *> expression) expression
+
+expression = atom <|> list
+    where
+      atom = variable <|> constant
+      list = parens (try lambda <|> try cons <|> application)
---
+++ Token.hs
@@ -0,0 +1,86 @@
+module VL.Token where
+
+import Data.Char
+
+data Token
+    = Identifier String
+    | Boolean Bool
+    | Real Float
+    | LParen
+    | RParen
+    | Quote
+      deriving (Eq, Show)
+
+scan :: String -> [Token]
+scan [] = []
+
+-- The lexical structure of identifiers follows rather closely that
+-- described in Section "7.1.1 Lexical structure" of R5RS.
+scan (c:cs)
+    | isInitial c
+    = Identifier (c:cs') : scan cs''
+    where
+      (cs', cs'') = span isSubsequent cs
+
+-- There are two peculiar identifiers: + and -, which require special
+-- care.  They are peculiar in that they may be part of a real.
+scan ('+':c:cs)
+    | not (isDigit c)
+    = Identifier "+" : scan (c:cs)
+    | otherwise
+    = (Real (read cs' :: Float) : scan cs'')
+    where
+      (cs', cs'') = scanReal (c:cs)
+scan ('-':c:cs)
+    | not (isDigit c)
+    = Identifier "-" : scan (c:cs)
+    | otherwise
+    = (Real (negate (read cs' :: Float)) : scan cs'')
+    where
+      (cs', cs'') = scanReal (c:cs)
+
+scan ('#':'t':cs) = Boolean True  : scan cs
+scan ('#':'f':cs) = Boolean False : scan cs
+scan ('('    :cs) = LParen        : scan cs
+scan (')'    :cs) = RParen        : scan cs
+scan ('\''   :cs) = Quote         : scan cs
+
+-- We support comments starting with ; and extending to the end of
+-- line.
+scan (';':cs) = scan (dropWhile (not . isNewline) cs)
+scan (c:cs) | isWhitespace c = scan cs
+
+scan _ = error "lexical analysis error"
+
+isInitial :: Char -> Bool
+isInitial c = isLetter c || isSpecialInitial c
+
+isSpecialInitial :: Char -> Bool
+isSpecialInitial c = c `elem` "!$%&*/:<=>?^_~"
+
+isSubsequent :: Char -> Bool
+isSubsequent c = isInitial c || isDigit c || isSpecialSubsequent c
+
+isSpecialSubsequent :: Char -> Bool
+isSpecialSubsequent c = c `elem` "+-.@"
+
+scanReal :: String -> (String, String)
+scanReal cs = (integralPart ++ fractionalPart, cs'')
+    where
+      (integralPart,   cs' ) = scanIntegralPart   cs
+      (fractionalPart, cs'') = scanFractionalPart cs'
+
+scanIntegralPart :: String -> (String, String)
+scanIntegralPart = span isDigit
+
+scanFractionalPart :: String -> (String, String)
+scanFractionalPart ('.':cs) = ('.':cs', cs'')
+    where
+      (cs', cs'') = span isDigit cs
+scanFractionalPart cs = ([], cs)
+
+isNewline :: Char -> Bool
+isNewline c = c == '\n'
+
+isWhitespace :: Char -> Bool
+isWhitespace c = isSpace c || isNewline c
