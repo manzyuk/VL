f708eef2f17395cf54d3081e451b6c8d6a124e6d
Oleksandr Manzyuk <manzyuk@googlemail.com>
2011-04-21 13:13:34 +0100

    The code generator is almost complete; alas, it doesn't work, even on pretty simple examples.
    It is not clear what to do with void values.  E.g., in
    
    ((lambda (x) x) (real 3))
    
    the closure is void (because its environment is empty), however, it
    yields non-void results when applied to the abstract real, so some
    code must be generated for it.  But it can't be!  Because the code
    would include a function that takes two arguments, a closure and a
    double, but there is no C type associated with the closure because
    it's void.

--- CodeGenerator.hs
+++ CodeGenerator.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE PatternGuards #-}
+{-# LANGUAGE PatternGuards, TypeOperators, FlexibleInstances #-}
 module VL.CodeGenerator where
 
 import VL.Common
 import VL.Scalar
+import VL.Coproduct
 import VL.Expression
+import VL.FixedPoint
 
 import VL.Environment (Environment)
 import qualified VL.Environment as Environment
@@ -32,6 +34,8 @@ import Control.Monad
 import Control.Arrow (first, second)
 
 import Text.PrettyPrint
+
+import Debug.Trace
 
 -- Types
 data CType
@@ -125,10 +129,13 @@ isNonVoid = not . isVoid
 
 -- Given a lookup /table/, return a lookup /function/.  The lookup
 -- function signals an error when the key is not found.
-mkLookupFun :: Ord k => Map k v -> (k -> v)
+mkLookupFun :: (Ord k, Pretty k) => Map k v -> (k -> v)
 mkLookupFun table key = fromMaybe (error msg) (Map.lookup key table)
     where
-      msg = "mkLookupFun: key is not found"
+      msg = "mkLookupFun: key is not found " ++ pprint key
+
+instance Pretty (AbstractValue, AbstractValue) where
+    pp (v1, v2) = pp v1 <> char ',' <+> pp v2
 
 -- Given a list of things, build a lookup table, giving each thing
 -- a unique name based on a prefix supplied as the first argument.
@@ -179,8 +186,91 @@ genCStructDecl tFun mFun v
 	 ]
     | otherwise = []
 
+genCExpr :: (Name -> Name)                           -- X
+	 -> (AbstractValue -> Name)                  -- M
+	 -> ((AbstractValue, AbstractValue) -> Name) -- F
+	 -> AbstractAnalysis                         -- a*
+	 -> AbstractEnvironment
+	 -> CoreExpression
+	 -> CExpr
+genCExpr xFun mFun fFun a env (In t)
+    = genCExprFromCoreExpr xFun mFun fFun a env t
+
+class GenCExprFromCoreExpr f where
+    genCExprFromCoreExpr :: (Name -> Name)                           -- X
+			 -> (AbstractValue -> Name)                  -- M
+			 -> ((AbstractValue, AbstractValue) -> Name) -- F
+			 -> AbstractAnalysis                         -- a*
+			 -> AbstractEnvironment
+			 -> f CoreExpression
+			 -> CExpr
+
+instance GenCExprFromCoreExpr Variable where
+    genCExprFromCoreExpr xFun mFun fFun a env (Variable x)
+	| x `elem` (Environment.domain env) -- x is bound in env
+	= CVariable (xFun x)
+	| otherwise
+	= CSlotAccess "c" (xFun x)
+
+instance GenCExprFromCoreExpr LambdaOneArg where
+    genCExprFromCoreExpr xFun mFun fFun a env (LambdaOneArg arg body)
+	= CFunctionCall name (map CVariable $ Environment.domain env)
+	where
+	  name = mFun $ Analysis.lookup (mkLambdaOneArg arg body) env a
+
+instance GenCExprFromCoreExpr ApplicationOneArg where
+    genCExprFromCoreExpr xFun mFun fFun a env (ApplicationOneArg operator operand)
+	= CFunctionCall name [ genCExpr xFun mFun fFun a env operator
+			     , genCExpr xFun mFun fFun a env operand
+			     ]
+	where
+	  name = fFun ( Analysis.lookup operator env a
+		      , Analysis.lookup operand  env a
+		      )
+
+instance GenCExprFromCoreExpr Cons where
+    genCExprFromCoreExpr xFun mFun fFun a env (Cons e1 e2)
+	= CFunctionCall name [ genCExpr xFun mFun fFun a env e1
+			     , genCExpr xFun mFun fFun a env e2
+			     ]
+	where
+	  name = mFun $ AbstractPair (Analysis.lookup e1 env a)
+				     (Analysis.lookup e2 env a)
+
+instance GenCExprFromCoreExpr LetrecOneArg where
+    genCExprFromCoreExpr xFun mFun fFun a env (LetrecOneArg bindings body)
+	= error "genCExprFromCoreExpr: LetrecOneArg is not supported yet"
+
+instance (GenCExprFromCoreExpr f, GenCExprFromCoreExpr g)
+    => GenCExprFromCoreExpr (f :+: g) where
+	genCExprFromCoreExpr xFun mFun fFun a env (Inl x)
+	    = genCExprFromCoreExpr xFun mFun fFun a env x
+	genCExprFromCoreExpr xFun mFun fFun a env (Inr x)
+	    = genCExprFromCoreExpr xFun mFun fFun a env x
+
+genFunctionDecl :: (Name -> Name)                           -- X
+		-> (AbstractValue -> CType)                 -- T
+		-> (AbstractValue -> Name)                  -- M
+		-> ((AbstractValue, AbstractValue) -> Name) -- F
+		-> AbstractAnalysis                         -- a*
+		-> AbstractValue
+		-> AbstractEnvironment
+		-> Name
+		-> CoreExpression
+		-> CDecl
+genFunctionDecl xFun tFun mFun fFun a v env x e
+    = CFunctionDecl ty name formals body
+    where
+      closure = AbstractClosure env x e
+      ty      = tFun $ refineApply closure v a
+      name    = fFun (closure, v)
+      formals = [ (tFun closure, "c")
+		, (tFun v, xFun x)
+		]
+      body    = [ CReturn $ genCExpr xFun mFun fFun a (Environment.insert x v env) e ]
+
 genCProg :: (CoreExpression, ScalarEnvironment) -> CProg
-genCProg program@(expression, constants) = structDecls
+genCProg program@(expression, constants) = structDecls ++ functionDecls
     where
       analysis = analyze program
       values   = Analysis.values analysis
@@ -197,4 +287,18 @@ genCProg program@(expression, constants) = structDecls
       mTbl = mkLookupTbl "#:con-" nonVoids
       mFun = mkLookupFun mTbl
 
+      nonVoidClosures = [ (env, x, e, v)
+			| c@(AbstractClosure env x e) <- values
+			, let vs = [ v'
+				   | v <- values
+				   , let v' = refineApply c v analysis
+				   , v' /= AbstractBottom
+				   ]
+			, all isNonVoid vs
+			, v <- vs
+			]
+
       structDecls = concatMap (genCStructDecl tFun mFun) nonVoids
+      functionDecls = [ genFunctionDecl xFun tFun mFun fFun analysis v env x e
+		      | (env, x, e, v) <- nonVoidClosures
+		      ]
