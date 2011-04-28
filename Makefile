76a29ef8b5a3184f2ea5775eb446d2a894a04286
Oleksandr Manzyuk <manzyuk@gmail.com>
2011-04-28 14:02:50 +0100

    Added a small test suite for the compiler.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-.PHONY: test clean vl2c
-test:
+.PHONY: test-evaluator test-compiler clean vl2c
+test-evaluator:
 	ghc --make -O2 -Wall  \
 		Test/Test.hs  \
 		Language/*.hs \
@@ -7,6 +7,8 @@ test:
 		Abstract/*.hs \
 		Concrete/*.hs \
 	-threaded -o test && ./test
+test-compiler:
+	Test/./run-tests.sh
 vl2c:
 	ghc --make -O2 -Wall  \
 		Compiler/*.hs \
@@ -17,5 +19,7 @@ vl2c:
 	-o vl2c
 clean:
 	rm -f test vl2c
-	find . -name "*.o"  -delete
-	find . -name "*.hi" -delete
+	find . -name "*.o"   -delete
+	find . -name "*.hi"  -delete
+	find . -name "*.c"   -delete
+	find . -name "*.out" -delete
---
+++ Test/run-tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DIR=$(pwd)
+
+if [ ! -e $DIR/vl2c ]
+then
+    echo "The file 'vl2c' not found.  Run 'make vl2c' first."
+    exit 1
+fi
+
+PASSED=0
+FAILED=0
+
+for file in $(ls Test/vl/*.vl)
+do
+    NAME=${file%.vl}
+    RECEIVED=$($DIR/vl2c $NAME.vl $NAME.c \
+        && gcc --param sra-field-structure-ratio=0 $NAME.c -o $NAME.out && ./$NAME.out)
+    EXPECTED=$(tail -1 $NAME.vl | tr -d ";\n ")
+    if [ $RECEIVED == $EXPECTED ]
+    then
+        printf "%-50s PASSED\n" $file
+        PASSED=$(($PASSED+1))
+    else
+        printf "%-50s FAILED\n" $file
+        echo "expected: $EXPECTED"
+        echo "received: $RECEIVED"
+        FAILED=$(($FAILED+1))
+    fi
+done
+
+TOTALLY=$(($PASSED+$FAILED))
+
+echo "Totally $TOTALLY test(s), $PASSED passed, $FAILED failed."
